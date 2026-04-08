e68638044419fee8ee3f70afc4280268c06be704
EsmyEsmyEsmy <contact@esmy.ai>
2026-04-08 23:10:41 +0200

    Add files via upload

--- pages/login.js
+++ pages/login.js
@@ -97,7 +97,7 @@ export default function Login() {
 
       <div className="card">
         <div className="logo">
-esmy<span style={{color:'rgba(255,255,255,.35)'}}>.</span>
+Esmy<span style={{color:'rgba(255,255,255,.35)',marginLeft:'-1px'}}>.</span>
         </div>
 
         <h1>{mode === 'login' ? 'Bon retour 👋' : 'Créer un compte'}</h1>
