76297515ea86de4fa5c8973ea7c006ad477f1e00
EsmyEsmyEsmy <contact@esmy.ai>
2026-04-09 21:50:59 +0200

    Add files via upload

--- pages/confirm.js
+++ pages/confirm.js
@@ -61,7 +61,7 @@ export default function Confirm() {
 
       <div className="card">
         <div className="logo">
-          Esmy<span style={{color:'rgba(255,255,255,.35)',marginLeft:'-1px'}}>.</span>
+          esmy<span style={{color:'rgba(255,255,255,.35)',marginLeft:'-3px',letterSpacing:'-.5px'}}>.</span>
         </div>
 
         {status === 'loading' && (
